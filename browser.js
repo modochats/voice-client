35785371a21bef02edd08b41abaeb146ab4bbf6b
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2026-01-06 17:39:16 +0330

    types & version

--- browser.js
+++ browser.js
@@ -1,8 +1,7 @@
 export {VoiceClient} from "./src/app.ts";
-export {EventType} from "./src/types/events.ts";
-export {LogLevel} from "./src/types/config.ts";
-export {AudioPlaybackState, RecordingState} from "./src/types/audio.ts";
-export {ConnectionState as WebSocketConnectionState} from "./src/types/websocket.ts";
-export * from "./src/types/index.ts";
+export {EventType} from "./src/services/shared/types/events.ts";
+export {AudioPlaybackState, RecordingState} from "./src/services/audio/audio.ts";
+export {ConnectionState as WebSocketConnectionState} from "./src/services/web-socket/websocket.ts";
+export * from "./src/services/shared/types/index.ts";
 export * from "./src/services/index.ts";
 export * from "./src/utils/index.ts";
